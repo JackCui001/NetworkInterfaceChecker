23ea037e9eca6c82aeeacd42db88e7db9252aaea
Jack Cui <443289353@qq.com>
2023-12-21 10:29:29 +0800

    Using Android View

--- app/src/main/java/com/jackcui/networkinterfacechecker/MainActivity.kt
+++ app/src/main/java/com/jackcui/networkinterfacechecker/MainActivity.kt
@@ -2,26 +2,68 @@ package com.jackcui.networkinterfacechecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import com.jackcui.networkinterfacechecker.databinding.ActivityMainBinding
+import java.lang.StringBuilder
 import java.net.NetworkInterface
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var vb: ActivityMainBinding
+    private var info = getNetworkInterfaceInfo()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // ViewBinding
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+
+        val keyList = info.keys.toList()
+        val adpt = ArrayAdapter(
+            this,
+            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+            keyList
+        )
+        vb.spnInterface.adapter = adpt
+        vb.spnInterface.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                vb.tvInfo.text = info[keyList[position]]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
     }
 
-    fun getNetworkInterfaceInfo(): MutableMap<String, String> {
+    private fun getNetworkInterfaceInfo(): MutableMap<String, String> {
         val interfaceInfo = hashMapOf<String, String>()
-        NetworkInterface.getNetworkInterfaces()?.let {
+        NetworkInterface.getNetworkInterfaces()?.let { it ->
             while (it.hasMoreElements()) {
                 val curInterface = it.nextElement()
-                val curInfo =
-                    "Display name: ${curInterface.displayName}\nHardware address: ${curInterface.hardwareAddress}\nInternet addresses: ${
-                        curInterface.inetAddresses.toList().joinToString()
-                    }\nIs up: ${curInterface.isUp}\nIs loopback: ${curInterface.isLoopback}\nIs virtual: ${curInterface.isVirtual}\nIs p2p: ${curInterface.isPointToPoint}"
-                interfaceInfo[curInterface.displayName] = curInfo
+                val curInfo = StringBuilder()
+                val inetAddr = curInterface.inetAddresses
+                val inetAddrSB = StringBuilder()
+                while (inetAddr.hasMoreElements()) {
+                    val curAddr = inetAddr.nextElement()
+                    curAddr.hostAddress?.let {
+                        inetAddrSB.append("$it\n")
+                    }
+                }
+                curInfo.append(
+                    "Internet addresses:\n $inetAddrSB\n\n"
+                )
+                curInfo.append("Is up:\n ${curInterface.isUp}\n\n")
+                curInfo.append("Is loopback:\n ${curInterface.isLoopback}\n\n")
+                curInfo.append("Is virtual:\n ${curInterface.isVirtual}\n\n")
+                curInfo.append("Is p2p:\n ${curInterface.isPointToPoint}\n\n")
+                interfaceInfo[curInterface.displayName] = curInfo.toString()
             }
         }
-        return interfaceInfo;
+        return interfaceInfo
     }
 }
